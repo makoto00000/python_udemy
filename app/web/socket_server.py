474281213fd2fe5506fde67da3ebdd20c6fdd08e
makoto hata <makotohata0429@gmail.com>
2024-09-04 21:20:01 +0900

    socket

---
+++ app/web/socket_server.py
@@ -0,0 +1,22 @@
+import socket
+
+# TCP
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind(('127.0.0.1', 50007))
+#     s.listen(1)
+#     while True:
+#         conn, addr = s.accept()
+#         with conn:
+#             while True:
+#                 data = conn.recv(1024)  # チャンク
+#                 if not data:
+#                     break
+#                 print('data: {}, addr: {}'.format(data, addr))
+#                 conn.sendall(b'Received: ' + data)
+
+# UDP
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind(('127.0.0.1', 50007))
+    while True:
+        data, addr = s.recvfrom(1024)
+        print("data: {}, addr: {}".format(data, addr))
