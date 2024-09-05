feb4392f17f995214aa9543818287dd6bb0ec880
makoto hata <makotohata0429@gmail.com>
2024-09-06 08:40:59 +0900

    pytest

---
+++ app/test/pytest_test.py
@@ -0,0 +1,49 @@
+import pytest
+
+import calculation
+
+is_release = True
+
+
+def test_add_num_and_double():
+    cal = calculation.Cal()
+    assert cal.add_num_and_double(1, 1) == 4
+
+
+class TestCal(object):
+    # このクラスのテスト開始前に実行
+    @classmethod
+    def setup_class(cls):
+        print('test start')
+        cls.cal = calculation.Cal()
+
+    # このクラスのテスト終了時に実行
+    @classmethod
+    def teardown_class(cls):
+        print('test end')
+        cls.cal = calculation.Cal()
+
+    # 各テストの前に実行
+    def setup_method(self, method):
+        print('setup method={}'.format(method.__name__))
+        # self.cal = calculation.Cal()
+
+    # 各テストの後に実行
+    def teardown_method(self, method):
+        print('teardown method={}'.format(method.__name__))
+        # del self.cal
+
+    # @pytest.mark.skip(reason='skip!')
+    @pytest.mark.skipif(is_release is True, reason='skip!')
+    def test_add_num_and_double(self):
+        assert self.cal.add_num_and_double(1, 1) == 4
+
+    # withの中でValueエラーが発生するかどうか
+    def test_add_num_and_double_raise(self):
+        with pytest.raises(ValueError):
+            self.cal.add_num_and_double('1', '1')
+
+# 「pytest -sv test/pytest_test.py」を実行する。
+
+# sオプション print文を表示
+# vオプション 詳細表示（skip表示）
