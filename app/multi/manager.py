012a3f4aa4d69e1cd2a5736b1897af4e1111583d
makoto hata <makotohata0429@gmail.com>
2024-09-11 11:39:59 +0900

    manager

---
+++ app/multi/manager.py
@@ -0,0 +1,34 @@
+import logging
+import multiprocessing
+
+logging.basicConfig(
+    level=logging.DEBUG, format='%(processName)s: %(message)s')
+
+
+def worker1():
+    l.reverse()
+    d['x'] += 1
+    n.y += 1
+
+
+if __name__ == '__main__':
+    with multiprocessing.Manager() as manager:
+        l = manager.list()
+        d = manager.dict()
+        n = manager.Namespace()
+
+        l.append(1)
+        l.append(2)
+        l.append(3)
+        d['x'] = 0
+        n.y = 0
+
+        p1 = multiprocessing.Process(target=worker1, args=(l, d, n))
+        p1.start()
+        p1.join()
+
+        logging.debug(l)
+        logging.debug(d)
+        logging.debug(n)
+
+# managerを使うとPythonオブジェクトのように値を使うことができるが、そのぶん遅くなる。その場合はValueやArrayを使用する。
